bf9228417f3a974180feaa99cbf54b45f6bc0bcc
lengcaiyoong <46414289+lengcaiyoong@users.noreply.github.com>
2019-02-04 16:44:11 +0800

    Update (multinom) tabulate continuous data.R

--- (multinom) tabulate continuous data.R
+++ (multinom) tabulate continuous data.R
@@ -53,7 +53,7 @@ nI      <-      # number of interval needed
   
     #example
 c1  <-  data00$x                    # eg. data00$x ; column of continuous variable
-nI  <-  7                           # number of interval needed 
+nI  <-  4                           # number of interval needed 
 profile <-data00$profile          # eg. data00[,1] ; assigned number of each obsrvatation 
 
 
@@ -97,7 +97,7 @@ z <- do.call(rbind,storage_3)
 sum(z)
 
 print(z)
-
+t(z)
 
 
 
@@ -107,8 +107,8 @@ plot(1:length(unique(data00$y)),z[1,],type="o",pch=1, col="red", ylim=c(0,max(z)
 
 
 #add extra lines
-    #examples of line 2 and 5
+    #examples of line 2 and 4
 lines(1:length(unique(data00$y)),z[2,],type="o",pch=3, col="blue")
-lines(1:length(unique(data00$y)),z[5,],type="o",pch=5, col="black")
+lines(1:length(unique(data00$y)),z[4,],type="o",pch=4, col="black")
 
 
