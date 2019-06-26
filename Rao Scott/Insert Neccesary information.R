6845565d804630b37b70e4f2f18ea714a5770112
lengcaiyoong <46414289+lengcaiyoong@users.noreply.github.com>
2019-06-27 00:09:57 +0800

    Add files via upload

---
+++ Rao Scott/Insert Neccesary information.R
@@ -0,0 +1,73 @@
+	#insert data
+print(noquote("Select your sampling frame"))
+readline(prompt="Press [enter] to continue")	
+s_frame <- read.csv(file.choose(),head=T, na.strings=c("","NA"))	#read sampling frame
+
+
+print(noquote("Select your sample data"))
+readline(prompt="Press [enter] to continue")	
+s_out <- read.csv(file.choose(),head=T, na.strings=c("","NA"))	#read sample data
+
+
+	#install and activate packages
+suppressWarnings(install.packages("dplyr"))
+suppressWarnings(library(dplyr))
+suppressWarnings(install.packages("DescTools"))
+suppressWarnings(library(DescTools))
+suppressWarnings(install.packages("sampling"))
+suppressWarnings(library(sampling))
+clc <- function() cat(rep("\n", 50))
+clc()
+
+
+
+	#create neccesary column
+s_frame <- mutate(s_frame, DEPT_CODE =  as.integer(substr(s_frame$PSEUDO.ID,4,6))   ) #create column of DEPT CODE
+s_frame   <- mutate( s_frame , YEAR = as.integer(substr(s_frame$PSEUDO.ID,2,3))  ) #create YEARS column in s_frame
+s_out <- mutate(s_out, DEPT_CODE =  as.integer(substr(s_out$PSEUDO.ID,4,6))   ) #create column of DEPT CODE
+s_out   <- mutate( s_out , YEAR = as.integer(substr(s_out$PSEUDO.ID,2,3))  ) #create YEARS column 
+strata <- sort(unique(s_out$DEPT_CODE))
+
+
+	#delete all NA column
+s_out <- s_out[, !apply(is.na(s_out), 2, all)]
+s_frame <- s_frame[, !apply(is.na(s_frame), 2, all)]
+
+
+	#sort s_out according to DEPT_CODE
+s_out_clswr <- s_out
+s_out <- s_out[order(s_out$DEPT_CODE),]
+
+
+
+	#ask to insert group variable
+print(noquote("How many levels of variable are there?"))
+n_variablelvl <- as.numeric(readline(noquote("Number of levels: ")))
+
+print(noquote("Enter them one by one"))
+
+variablelvl <- rep(NA,n_variablelvl)
+
+for (i in 1:n_variablelvl){
+	variablelvl[i] <- readline(noquote("Enter Variable Levels:"))
+				}
+
+variable_lvl <- factor(variablelvl, levels=c(variablelvl))
+
+
+
+
+	#set factor order
+s_out$IDENTITY.STATUS <- factor(s_out$IDENTITY.STATUS, levels = variable_lvl)
+
+
+
+
+	#copy same s_out for random group SRS
+s_out_rgsrs <- s_out
+s_out_rgstr <- s_out
+
+
+
+
+
