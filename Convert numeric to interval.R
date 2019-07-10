5aa92aca5de716080246c696b26c150586830d07
lengcaiyoong <46414289+lengcaiyoong@users.noreply.github.com>
2019-07-10 21:32:51 +0800

    Update Convert numeric to interval.R

--- Convert numeric to interval.R
+++ Convert numeric to interval.R
@@ -15,6 +15,6 @@ Assign = function(df,v,interval){   #df = data frame ; v stands for which column
 #Example
 dat = data.frame(A=runif(10,1,10), B=runif(10,2,9))  #data
 group = c(5,10)                           #wanna group column 1 into <= 5 or more than 5
-k <- Assign(dat,1,interval)               #v = 1
-print(k)
+dat <- Assign(dat,1,interval)               #v = 1
+dat
 
