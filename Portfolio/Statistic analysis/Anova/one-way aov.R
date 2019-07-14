e5767a137bb9cda7de1c051ce9e50851b8143d6f
lengcaiyoong <46414289+lengcaiyoong@users.noreply.github.com>
2019-07-15 01:49:47 +0800

    Update one-way aov.R

--- Portfolio/Statistic analysis/Anova/one-way aov.R
+++ Portfolio/Statistic analysis/Anova/one-way aov.R
@@ -3,48 +3,43 @@
 
 
 
-#Enter Data
+#1 Enter Data
 dat = read.table(url("http://www.uvm.edu/~dhowell/methods8/DataFiles/Tab11-2.dat"),head=T)
 
-str(dat)  #check if explanatory variables are FACTOR
+str(dat)                              #check if explanatory variables are FACTOR
 dat$Group <- factor(dat$Group)
 
 
 
-
-#Means of each group
+#2 Means of each group
 dat_mean <- model.tables(anova1,type="means",data=dat)
 
 
-#10 post hoc pairwise test in total
+
+#3 Ten post hoc pairwise tests in total
 levels(dat$Group)   #levels of explanatory variable
 dim(combn(seq(0,8,2),2))[2]   #how many pairwise comparison can be conducted
 
 
 
-#Run Anova
+#4 Run Anova
 anova1 <- aov(dv~Group, data=dat)
 
 
-#Plot Graph
+
+#5 Plot Graph
 plot(dat$dv~dat$Group)
 abline(h=mean(dat$dv),col="red")
 
 
 
 
+####          Interpretation           ###
 
-
-
-#Interpretation
 #01   Result is significant! 
 #
 
 summary(anova1)
-
-
-
-
 
 
 
@@ -56,9 +51,7 @@ summary(anova1)
 #(a) Critical value (in terms of mean difference)
 #     Group means difference should be more than 1.732 in order to show significance   
 
-
 qtukey(0.95,5,df=55)*sqrt(summary(anova1)[1][[1]][[3]][[2]]/table(dat$Group)[[1]])
-
 dat_mean$tables$Group[order(dat_mean$tables$Group)] #sort the means in ascending order
      #(4) =/= (0,8)
 
@@ -77,15 +70,8 @@ ptukey(6.451561,5,55,lower.tail=F)       # pv <.05
 
 
 
-
-
-
-
 #(c) All pairwise test
 #     The results should be identical with the calculation above
 
 TukeyHSD(anova1)
-
-
-
 
